496a92e202cf8698faa47c83acdd9474fa8c3ef1
ArushSagar15 <25110cn025@glbitm.ac.in>
2026-06-07 17:25:26 +0530

    Add Day 1 files

---
+++ Day 1/SumOfNaturalNumbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SumOfNaturalNumbers
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int n, sum = 0;
+
+        System.out.print("Enter N: ");
+        n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++)
+        {
+            sum = sum + i;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
